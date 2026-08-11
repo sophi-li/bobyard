6a8b48367e0a69599407ee0b158bfb06a418e37a
sophi-li <sophiali124@gmail.com>
2026-08-10 22:16:08 -1000

    pull orderComments into own file

---
+++ bobyard-fe/src/orderComments.js
@@ -0,0 +1,38 @@
+export const orderComments = (comments) => {
+  let groupedChildrenByParentId = {};
+
+  // group all the children by parentId
+  for (let i = 0; i < comments.length; i++) {
+    let currComment = comments[i];
+    let parentId = currComment.parent;
+    if (groupedChildrenByParentId[parentId] === undefined) {
+      groupedChildrenByParentId[parentId] = [];
+    }
+    groupedChildrenByParentId[parentId].push(currComment);
+  }
+
+  let sortedComments = [];
+
+  // keep adding children
+  const addComment = (comment, depth) => {
+    sortedComments.push({ ...comment, depth });
+
+    let children = groupedChildrenByParentId[comment.id];
+    if (children !== undefined) {
+      for (let k = 0; k < children.length; k++) {
+        // grandchildren
+        addComment(children[k], depth + 1);
+      }
+    }
+  };
+
+  // start with top level parents
+  let topLevelParents = groupedChildrenByParentId[''];
+  if (topLevelParents !== undefined) {
+    for (let j = 0; j < topLevelParents.length; j++) {
+      addComment(topLevelParents[j], 0);
+    }
+  }
+
+  return sortedComments;
+};
--- bobyard-fe/src/useComments.js
+++ bobyard-fe/src/useComments.js
@@ -1,44 +1,6 @@
 import { useState, useEffect } from 'react';
 import { createComment, getComments } from './api/comments.js';
-
-const orderComments = (comments) => {
-  let groupedChildrenByParentId = {};
-
-  // group all the children by parentId
-  for (let i = 0; i < comments.length; i++) {
-    let currComment = comments[i];
-    let parentId = currComment.parent;
-    if (groupedChildrenByParentId[parentId] === undefined) {
-      groupedChildrenByParentId[parentId] = [];
-    }
-    groupedChildrenByParentId[parentId].push(currComment);
-  }
-
-  let sortedComments = [];
-
-  // keep adding children
-  const addComment = (comment, depth) => {
-    sortedComments.push({ ...comment, depth });
-
-    let children = groupedChildrenByParentId[comment.id];
-    if (children !== undefined) {
-      for (let k = 0; k < children.length; k++) {
-        // grandchildren
-        addComment(children[k], depth + 1);
-      }
-    }
-  };
-
-  // start with top level parents
-  let topLevelParents = groupedChildrenByParentId[''];
-  if (topLevelParents !== undefined) {
-    for (let j = 0; j < topLevelParents.length; j++) {
-      addComment(topLevelParents[j], 0);
-    }
-  }
-
-  return sortedComments;
-};
+import { orderComments } from './orderComments.js';
 
 export function useComments() {
   // TODO: Add loading and error states
