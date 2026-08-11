261701b3d60cafc59fd6e8f8c6891ca4afb834f4
sophi-li <sophiali124@gmail.com>
2026-08-10 22:13:18 -1000

    add fallback avatar for broken avatar link

--- bobyard-fe/src/Comment.jsx
+++ bobyard-fe/src/Comment.jsx
@@ -4,7 +4,6 @@ import { timeAgo } from './timeAgo.js';
 
 export function CommentContainer({ comment }) {
   const { author, parent, id, depth, text, likes } = comment;
-  // TODO: Validate avatar url is successful
   let avatar = comment.image !== '' ? comment.image : anonAvatar;
   let time = timeAgo(comment.date);
   let depthPx = `${depth * 30}px`;
@@ -12,7 +11,15 @@ export function CommentContainer({ comment }) {
   return (
     <div className="commentContainer" style={{ marginLeft: depthPx }}>
       <div className="imgContainer">
-        <img src={avatar} className="avatar" alt={`${author}'s avatar`}></img>
+        <img
+          src={avatar}
+          className="avatar"
+          alt={`${author}'s avatar`}
+          onError={(e) => {
+            e.currentTarget.onerror = null; // Prevents infinite loops if fallback also fails
+            e.currentTarget.src = anonAvatar;
+          }}
+        ></img>
       </div>
       <div className="commentBody">
         <div className="headerRow">
